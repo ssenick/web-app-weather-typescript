3ccfc3b580ff59cd198b633379419f52fef60196
ssenick <ssenick@ukr.net>
2023-08-08 20:26:35 -0600

    Structure change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import React, {FC, useEffect} from 'react';
-import {Forecast, Header, Search} from "./components";
+import React, {FC, useEffect, useState} from 'react';
+import {Forecast, Header, Loader, Search} from "./components";
 import './styles/App.scss'
 import useSearchForecast from "./hooks/useSearchForecast";
+import {AllData} from "./type";
 
 
 const App: FC = () => {
     const dataForecast = useSearchForecast()
+    const [forecastData,setForecastData] = useState<AllData | null>(null);
     useEffect(()=>{
         console.log(dataForecast.allData)
+        setForecastData(dataForecast.allData)
     },[dataForecast.allData])
     return (
         <div className='app'>
-            <div
-                className='app__container'>
+            <div className='app__container'>
                 <div className="app__wrapper">
-                <Header/>
-                <main className='main'>
-                    <Search {...dataForecast}/>
-                    <Forecast/>
+                    <div className="app__header">
+                        <Header/>
+                        <Search {...dataForecast}/>
+                    </div>
+
+                <main className='app__main'>
+                    {dataForecast.isLoadingAllData && <Loader/>}
+                    {forecastData &&  <Forecast {...forecastData} />}
                 </main>
 
                 </div>
