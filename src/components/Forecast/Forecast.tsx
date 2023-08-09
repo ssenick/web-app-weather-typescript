7fcc5164ab2f983cc2afcaf97ae7c5f94bbe7424
ssenick <ssenick@ukr.net>
2023-08-08 20:27:25 -0600

    adding variables

--- src/components/Forecast/Forecast.tsx
+++ src/components/Forecast/Forecast.tsx
@@ -1,75 +1,58 @@
-import React from 'react';
+import React, {FC, memo} from 'react';
 import Degree from "../UI/Degree/Degree";
 import './Forecast.scss'
+import Sunrise from "../Icons/Sunrise";
+import Sunset from "../Icons/Sunset";
+import Card from "../Card/Card";
+import {AllData} from "../../type";
+import {getDate} from "../../helpers";
 
-const Forecast = () => {
-    const number = 21;
+
+
+const Forecast:FC<AllData> = memo((data) => {
+    const today = data.weather
+    const forecast = data.forecast
+    const icon = data.forecast.list[0].weather[0].icon
     return (
         <div className='forecast'>
             <section className="forecast__header">
-                <h2 className="forecast__title">London <span>GB</span></h2>
-                <img src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                <div className="forecast__temp">24</div>
-                <p className="forecast__weather">Smoke</p>
+                <h2 className="forecast__title">{today.name} <span>{today.sys.country}</span></h2>
+                <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=""/>
+                <div className="forecast__temp">{Math.round(today.main.temp)}</div>
+                <p className="forecast__weather">{today.weather[0].main}, {today.weather[0].description}</p>
                 <div className="forecast__temperature-range">
-                    <p className="forecast__high-temp">H: <span><Degree temp={number}/></span></p>
-                    <p className="forecast__low-temp">L: <span><Degree temp={number}/></span></p>
+                    <p className="forecast__high-temp">H: <span><Degree temp={Math.ceil(today.main.temp_max)}/></span></p>
+                    <p className="forecast__low-temp">L: <span><Degree temp={Math.floor(today.main.temp_min)}/></span></p>
                 </div>
             </section>
-            <section className="forecast__main ">
-                <div className="days">
-                    <ul className="days__list">
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
+            <section className="forecast__days days">
+                <ul className="days__list">
+                    {forecast.list.map((el,index) =>
+                        <li key={el.dt_txt} className="days__item">
+                            <div className="days__time">{index === 0 ? 'Now' : getDate(el.dt_txt)}</div>
+                            <img className="days__icon" src={`http://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`} alt=""/>
+                            <div className="days__temp"><Degree temp={Math.round(el.main.temp)}/></div>
                         </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
-                        <li className="days__item">
-                            <div className="days__time">19</div>
-                            <img className="days__icon" src={`http://openweathermap.org/img/wn/04d@2x.png`} alt=""/>
-                            <div className="days__temp">23</div>
-                        </li>
+                    )}
 
-                    </ul>
+
+                </ul>
+            </section>
+            <section className="forecast__sun-time sun-time">
+                <div className="sun-time__item">
+                    <Sunrise/>
+                    <p className="sun-time__time">07:00</p>
                 </div>
+                <div className="sun-time__item">
+                    <Sunset/>
+                    <p className="sun-time__time">18:00</p>
+                </div>
+            </section>
+            <section className="forecast__cards cards">
+                <Card icon='wind' title='Wind' info='asd' description='asdasdsa' />
             </section>
         </div>
     );
-};
+});
 
 export default Forecast;
