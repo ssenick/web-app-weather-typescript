894b55fadf50300acd7a36508ed2291af308577e
ssenick <ssenick@ukr.net>
2023-08-10 21:27:56 -0600

    add new types

--- src/type/index.ts
+++ src/type/index.ts
@@ -19,44 +19,80 @@ type ListItem = {
     ]
 }
 
-export type AllData = {
-    forecast: {
-        list: ListItem[]
-    },
-    weather: {
-        name: string,
-        clouds: { all: number }
-        sys: {
-            country: string,
-            sunrise: number,
-            sunset: number
+export type DailyItem = {
+    dt: number,
+    pop:number,
+    temp: {
+        day:number,
+        min:number,
+        max:number
+    }
+    weather: [
+        {
+            description: string,
+            icon: string,
+            main: string
         }
-        main: {
-            feels_like: number,
-            humidity: number,
-            pressure: number,
-            temp: number,
-            temp_max: number,
-            temp_min: number
-
-        },
-        visibility: number,
-        weather: [
-            {
-                description: string,
-                icon: string,
-                main: string
-            }
-        ],
-        wind: {
-            speed: number,
-            deg: number,
-            gust: number
-        },
-        dt:number
-    },
-    status: boolean
+    ]
 }
+export type AllData = {
+    current: {
+        clouds:number,
+        dt: number,
+        feels_like: number,
+        humidity: number,
+        pressure: number,
+        sunrise: number,
+        sunset: number,
+        temp: number,
+        visibility: number,
+        wind_deg: number,
+        wind_gust: number,
+        wind_speed: number
+    },
+    timezone:string
+
+    daily: DailyItem[],
+}
+// export type AllData = {
+//     forecast: {
+//         list: ListItem[]
+//     },
+//     weather: {
+//         name: string,
+//         clouds: { all: number }
+//         sys: {
+//             country: string,
+//             sunrise: number,
+//             sunset: number
+//         }
+//         main: {
+//             feels_like: number,
+//             humidity: number,
+//             pressure: number,
+//             temp: number,
+//             temp_max: number,
+//             temp_min: number
+//
+//         },
+//         visibility: number,
+//         weather: [
+//             {
+//                 description: string,
+//                 icon: string,
+//                 main: string
+//             }
+//         ],
+//         wind: {
+//             speed: number,
+//             deg: number,
+//             gust: number
+//         },
+//         dt:number
+//     },
+//     status: boolean
+// }
+//
 
 
 export type DataForecast = {
