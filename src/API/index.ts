5525d7a5d2b7ee37089d0125a51d96d87a6d54b4
ssenick <ssenick@ukr.net>
2023-08-10 21:26:34 -0600

    add new path and key

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,5 +1,7 @@
 
 export const API_KEY = '9c524706dbc1b41c216d1d7350c414fd';
+export const API_KEY_NOT_GOOD='1fa9ff4126d95b8db54f3897a208e91c'
 export const GLOBAL_CITY = 'https://api.openweathermap.org/geo/1.0/direct';
 export const GLOBAL_WEATHER = 'https://api.openweathermap.org/data/2.5/weather';
 export const GLOBAL_FORECAST = 'https://api.openweathermap.org/data/2.5/forecast';
+export const GLOBAL_TEST = 'https://api.openweathermap.org/data/2.5/onecall'
