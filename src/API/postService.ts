00e895215059596ff1b9a7c81a6fa6b0f424995e
ssenick <ssenick@ukr.net>
2023-08-10 21:26:53 -0600

    add new getForecast

--- src/API/postService.ts
+++ src/API/postService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {GLOBAL_CITY, GLOBAL_FORECAST, API_KEY, GLOBAL_WEATHER} from "./index";
+import {GLOBAL_CITY, API_KEY, GLOBAL_TEST, API_KEY_NOT_GOOD} from "./index";
 import {AllData, GlobalOption} from "../type";
 
 export default class PostService {
@@ -8,28 +8,35 @@ export default class PostService {
         return data
     }
 
-    // static async getWeather({lat, lon}: { lat: string, lon: string }) {
-    //     const {data} = await axios.get<GlobalOption[]>(`${GLOBAL_WEATHER}?lat=${lat}&lon=${lon}&units=metric&cnt=16&appid=${API_KEY}`);
-    //     return data
-    // }
-    //
-    // static async getForecast({lat, lon}: { lat: string, lon: string }) {
-    //     const {data} = await axios.get<GlobalOption[]>(`${GLOBAL_FORECAST}?lat=${lat}&lon=${lon}&units=metric&cnt=16&appid=${API_KEY}`);
-    //     return data
-    // }
-
-    static async getAll({lat, lon}: { lat: string, lon: string }) {
-        const endpoints = [
-            `${GLOBAL_WEATHER}?lat=${lat}&lon=${lon}&units=metric&cnt=16&lang=en&appid=${API_KEY}`,
-            `${GLOBAL_FORECAST}?lat=${lat}&lon=${lon}&units=metric&cnt=16&lang=en&appid=${API_KEY}`
-        ]
-
-        const response = await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
-
-        return {
-            'weather': response[0].data,
-            'forecast': response[1].data,
-            status:true
-        } as AllData
+    static async getForecast({lat, lon}: { lat: string, lon: string }) {
+        const {data} = await axios.get<AllData>(`${GLOBAL_TEST}?lat=${lat}&lon=${lon}&exclude=hourly&units=metric&appid=${API_KEY_NOT_GOOD}`);
+        return data
     }
 }
+
+
+
+// static async getWeather({lat, lon}: { lat: string, lon: string }) {
+//     const {data} = await axios.get<GlobalOption[]>(`${GLOBAL_WEATHER}?lat=${lat}&lon=${lon}&units=metric&cnt=16&appid=${API_KEY}`);
+//     return data
+// }
+//
+// static async getForecast({lat, lon}: { lat: string, lon: string }) {
+//     const {data} = await axios.get<GlobalOption[]>(`${GLOBAL_FORECAST}?lat=${lat}&lon=${lon}&units=metric&cnt=16&appid=${API_KEY}`);
+//     return data
+// }
+
+// static async getAll({lat, lon}: { lat: string, lon: string }) {
+//     const endpoints = [
+//         `${GLOBAL_WEATHER}?lat=${lat}&lon=${lon}&units=metric&cnt=16&lang=en&appid=${API_KEY}`,
+//         `${GLOBAL_FORECAST}?lat=${lat}&lon=${lon}&units=metric&cnt=16&lang=en&appid=${API_KEY}`
+//     ]
+//
+//     const response = await axios.all(endpoints.map((endpoint) => axios.get(endpoint)))
+//
+//     return {
+//         'weather': response[0].data,
+//         'forecast': response[1].data,
+//         status:true
+//     } as AllData
+// }
